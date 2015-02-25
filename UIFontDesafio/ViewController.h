696fa3807254362e67cc6ee19c50aef9d62752c8
Wellington Pardim Ferreira <wellingtons_10@hotmail.com>
2015-02-24 21:36:07 -0300

    tudo funcionando

--- UIFontDesafio/ViewController.h
+++ UIFontDesafio/ViewController.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    NSString *nomeFonte;
+}
 
 
+- (IBAction)Alterar:(id)sender;
+@property NSString *nomeFonte;
 @end
 
--- UIFontDesafio/ViewController.m
+++ UIFontDesafio/ViewController.m
@@ -9,10 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *Texto;
+@property (weak, nonatomic) IBOutlet UILabel *nomeLabel;
 
 @end
 
 @implementation ViewController
+@synthesize Texto, nomeLabel, nomeFonte;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,5 +26,30 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [Texto resignFirstResponder];
+}
+- (IBAction)Alterar:(id)sender {
+   [nomeLabel setFont:[UIFont fontWithName:nomeFonte size:14]];
+}
+
+
+- (IBAction)Helvetica:(id)sender {
+     nomeFonte = @"Helvetica";
+}
+                
+- (IBAction)Zaptinos:(id)sender {
+    nomeFonte= @"Zapfino";
+    
+}
+- (IBAction)Papyrus:(id)sender {
+    nomeFonte= @"Papyrus";
+}
+- (IBAction)CourierNew:(id)sender {
+    nomeFonte= @"Courier New";
+}
+
 
 @end
